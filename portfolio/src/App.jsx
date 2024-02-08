642b288d9df831c20c2a0461b2e398e67a603e9c
Headbreakerr <133957591+Headbreakar@users.noreply.github.com>
2024-02-08 18:13:19 +0500

    made the components
    Indented basic CSS for App component and initialized all the components.

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './components/header/header';
+import Nav from './components/nav/nav';
+import About from './components/about/about';
+import Portfolio from './components/portfolio/portfolio';
+import Contact from './components/contact/contact';
+import Experience from './components/experience/experience';
+import Footer from './components/footer/footer';
+import Services from './components/Services/services';
+import Testimonials from './components/testimonials/testimonials';
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Nav />
+    <About/>
+    <Experience/>
+    <Services />
+    <Portfolio />
+    <Testimonials />
+    <Contact />
+    <Footer/>
+    </>
+  );
+}
+
+export default App
