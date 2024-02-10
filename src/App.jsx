a2853541a404a4394162e9eaa7974e2bb20dec0f
Headbreakerr <133957591+Headbreakar@users.noreply.github.com>
2024-02-10 15:35:55 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import Header from './components/header/header';
-import Nav from './components/nav/nav';
-import About from './components/about/about';
-import Portfolio from './components/portfolio/portfolio';
-import Contact from './components/contact/contact';
-import Experience from './components/experience/experience';
-import Footer from './components/footer/footer';
-import Services from './components/Services/services';
-import Testimonials from './components/testimonials/testimonials';
+import React from "react";
+import Header from "./components/header/header";
+import Nav from "./components/nav/nav";
+import About from "./components/about/about";
+import Portfolio from "./components/portfolio/portfolio";
+import Contact from "./components/contact/contact";
+import Experience from "./components/experience/experience";
+import Footer from "./components/footer/footer";
+import Services from "./components/Services/services";
+import Testimonials from "./components/testimonials/testimonials";
 
 const App = () => {
   return (
     <>
-    <Header />
-    <Nav />
-    <About/>
-    <Experience/>
-    <Services />
-    <Portfolio />
-    <Testimonials />
-    <Contact />
-    <Footer/>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi,
+        inventore.
+      </p>
     </>
   );
-}
+};
 
-export default App
+export default App;
